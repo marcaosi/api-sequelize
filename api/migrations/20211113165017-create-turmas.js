c5b18b1ec1ef860846f79b9f1dd82c66564c152a
Marcos Antonio dos Santos <marcosantonio.dev@gmail.com>
2021-11-13 14:17:58 -0300

    add more tables to database

---
+++ api/migrations/20211113165017-create-turmas.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Turmas', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      data_inicio: {
+        type: Sequelize.DATEONLY
+      },
+      docente_Id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        refences: { model: 'Pessoas', key: 'id' }
+      },
+      nivel_Id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        refences: { model: 'Nivel', key: 'id' }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Turmas');
+  }
+};
