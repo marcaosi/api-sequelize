e12da0df69475955980fe220df5a76ffab5524b3
Marcos Antonio dos Santos <marcosantonio.dev@gmail.com>
2021-11-13 12:05:35 -0300

    creating put method

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -35,6 +35,28 @@ class PessoaController{
             return res.status(500).json(err.message)
         }
     }
+
+    static async atualizaPessoa(req, res){
+        const { id } = req.params
+        const novasInfos = req.body
+        try{
+            await database.Pessoas.update(novasInfos, {
+                where: {
+                    id: Number(id)
+                }
+            })
+            
+            const pessoaAtualizada = await database.Pessoas.findOne({
+                where: {
+                    id: Number(id)
+                }
+            })
+
+            return res.status(200).json(pessoaAtualizada)
+        }catch(err){
+            return res.status(500).json(err.message)
+        }
+    }
 }
 
 module.exports = PessoaController
