36cd8525441c5c0484e547d7677e52a099ea1d01
Marcos Antonio dos Santos <marcosantonio.dev@gmail.com>
2021-11-13 11:57:14 -0300

    creating get by id method

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -10,6 +10,21 @@ class PessoaController{
             return res.status(500).json(err.message)
         }
     }
+
+    static async pegaUmaPessoa(req, res){
+        const { id } = req.params
+        try{
+            const pessoa = await database.Pessoas.findOne({
+                where: {
+                    id: Number(id)
+                }
+            })
+            return res.status(200).json(pessoa)
+
+        }catch(err){
+            return res.status(500).json(err.message)
+        }
+    }
 }
 
 module.exports = PessoaController
