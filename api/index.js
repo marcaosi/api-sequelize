2471e5ebc7e1ff276a144bbb46f563f328b9a2e1
Marcos Antonio dos Santos <marcosantonio.dev@gmail.com>
2021-11-13 11:53:24 -0300

    creating PessoaController and write get method

--- api/index.js
+++ api/index.js
@@ -1,18 +1,10 @@
-const express = require("express")
-const bodyParser = require("body-parser")
+const express = require('express')
+const routes = require('./routes')
 
 const app = express()
-
-app.use(bodyParser.json())
-
 const port = 3000
 
-app.get('/teste', (req, res) => {
-    res.status(200)
-        .send({
-            mensage: 'boas vindas à API'
-        })
-})
+routes(app)
 
 app.listen(port, () => console.log('Servidor está rodando na porta: ' + port))
 
