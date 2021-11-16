0f48915156ab693ed1bee9f0dd7fc621cae12cd1
Marcos Antonio dos Santos <marcosantonio@fuvs.br>
2021-11-16 13:17:24 -0300

    add soft delete on tables and endpoint to restore people

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -138,6 +138,21 @@ class PessoaController{
             return res.status(500).json(err.message)
         }
     }
+
+    static async restauraPessoa(req, res){
+        const { id } = req.params
+        try{
+            await database.Pessoas.restore({
+                where: {
+                    id: Number(id)
+                }
+            })
+
+            return res.status(200).json({ ok: true })
+        }catch(err){
+            return res.status(500).json(err.message)
+        }
+    }
 }
 
 module.exports = PessoaController
